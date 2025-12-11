2666b61b098122726dff8d2710dd1897198c519a
HenrikSchuette <Henrik.schuette@gmx.de>
2025-12-11 09:11:23 +0100

    initial commit2

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "useless-extension" is now active!');
+	console.log('Congratulations, you activated the "useless-extension"!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
